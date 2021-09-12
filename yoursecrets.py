6a4781414a3b6f2ae577fe33566bb843c6537c35
Ikbel Boulabiar <boulabiar@gmail.com>
2021-09-12 13:02:48 +0200

    add secrets file template

---
+++ yoursecrets.py
@@ -0,0 +1,5 @@
+# Rename this file to mysecrets.py and put your twitch dev informations to connect to the API
+
+client_id = 'ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ'
+secret  =   'ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ'
+access_token = 'ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ'
